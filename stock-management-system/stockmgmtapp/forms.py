9b33afee15f75535a193508639288c80ed011b19
shemajolivetgislain <yshemajolivet@gmail.com>
2022-02-24 17:32:07 +0200

    form validations

--- stock-management-system/stockmgmtapp/forms.py
+++ stock-management-system/stockmgmtapp/forms.py
@@ -7,6 +7,21 @@ class StockCreateForm(forms.ModelForm):
         model = Stock
         fields = ['category', 'item_name', 'quantity']
 
+    def clean_category(self):
+        category = self.cleaned_data.get('category')
+        if not category:
+            raise forms.ValidationError('This field is required')
+        for instance in Stock.objects.all():
+            if instance.category == category:
+                raise forms.ValidationError(category + 'is already exist')
+        return category
+
+    def clean_item_name(self):
+        item_name = self.cleaned_data.get('item_name')
+        if not item_name:
+            raise forms.ValidationError('This field is required')
+        return item_name
+
 
 class StockSearchForm(forms.ModelForm):
     class Meta:
